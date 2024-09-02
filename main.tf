c66a4b0678925bc690330edef55755a5abd54b61
ioa6870_hca <blake.crozier@hcahealthcare.com>
2024-09-02 14:28:58 -0500

    Commit TF Files

---
+++ main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+} 
+
+provider "google" {
+  # credentials = file(var.credentials_file)
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "budget_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "us-east4"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bqowner.email
+  }
+
+  access {
+    role   = "READER"
+    domain = "hashicorp.com"
+  }
+}
+
+resource "google_service_account" "bqowner" {
+  account_id = "bqowner"
+}
